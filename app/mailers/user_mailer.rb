13a1deb84d37d7959630dd13b3452c21d1bebb50
Andre.Lee <andrelee082@gmail.com>
2015-01-09 09:22:50 -0800

    Fix mandrill caller.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,38 +7,38 @@ class UserMailer < MandrillMailer::TemplateMailer
 
   def welcome(user)
     mandrill do
-      mandrill template: 'welcome',
-               subject: 'Thank you for joining our community!',
-               to: {email: user.email, name: user.name},
-               inline_css: true,
-               async: true,
-               headers: {'Reply-To' => DEFAULT_REPLY_TO}
+      mandrill_mail template: 'welcome',
+                    subject: 'Thank you for joining our community!',
+                    to: {email: user.email, name: user.name},
+                    inline_css: true,
+                    async: true,
+                    headers: {'Reply-To' => DEFAULT_REPLY_TO}
     end
   end
 
   def booking_reminder(booking)
     mandrill do
-      mandrill template: 'appointment-reminder',
-               subject: 'Just a friendly reminder about your services tomorrow.',
-               to: {email: booking.property.user.email, name: booking.property.user.name},
-               vars: {'ADDR' => booking.property.short_address},
-               inline_css: true,
-               async: true,
-               headers: {'Reply-To' => DEFAULT_REPLY_TO}
+      mandrill_mail template: 'appointment-reminder',
+                    subject: 'Just a friendly reminder about your services tomorrow.',
+                    to: {email: booking.property.user.email, name: booking.property.user.name},
+                    vars: {'ADDR' => booking.property.short_address},
+                    inline_css: true,
+                    async: true,
+                    headers: {'Reply-To' => DEFAULT_REPLY_TO}
     end
   end
 
   def booking_confirmation(booking)
     mandrill do
       appt_date = booking.date.strftime('%b %e/%Y')
-      mandrill template: 'booking-confirmation',
-               subject: "Your services have been booked on #{appt_date} at #{booking.property.short_address}",
-               to: {email: booking.property.user.email, name: booking.property.user.name},
-               vars: {'ADDR' => booking.property.short_address,
-                      'DATE' => appt_date},
-               inline_css: true,
-               async: true,
-               headers: {'Reply-To' => DEFAULT_REPLY_TO}
+      mandrill_mail template: 'booking-confirmation',
+                    subject: "Your services have been booked on #{appt_date} at #{booking.property.short_address}",
+                    to: {email: booking.property.user.email, name: booking.property.user.name},
+                    vars: {'ADDR' => booking.property.short_address,
+                           'DATE' => appt_date},
+                    inline_css: true,
+                    async: true,
+                    headers: {'Reply-To' => DEFAULT_REPLY_TO}
     end
   end
 
