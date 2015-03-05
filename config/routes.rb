4c3d2000b785a13b3bf8f318497ac07958f33609
Chris Bolton <c@chrisbolton.me>
2015-03-05 10:27:44 -0800

    signout_url

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  default_url_options Rails.application.config.action_mailer.default_url_options
+
   get '/auth' => 'auth#auth', as: 'auth'
 
   post '/password_resets' => 'password_resets#create'
