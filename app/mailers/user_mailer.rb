c2351099cd320e93a8e3246d9b6886d5ac00f6ab
Chris Bolton <c@chrisbolton.me>
2015-03-06 15:19:21 -0800

    new booking notification update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -22,8 +22,13 @@ class UserMailer < MandrillMailer::TemplateMailer
                     subject: "Turn Needed on #{booking.date.strftime}",
                     to: {email: Rails.application.config.booking_notification_email},
                     vars: {
-                      'ID' => booking.id, 'NAME' => booking.property.user.name, 'NICKNAME' => booking.property.nickname,
-                      'FULL_ADDRESS' => booking.property.nickname, 'PROP_SIZE' => booking.property.property_type,
+                      'ID' => booking.id, 'CUST_NAME' => booking.property.user.name, 'CUST_PHONE' => booking.property.user.display_phone_number,
+                      'CUST_EMAIL' => booking.property.user.email, 'FULL_ADDRESS' => booking.property.full_address,
+                      'PROP_SIZE' => booking.property.property_type, 'SERVICE_DATE' => booking.date.strftime,
+                      'NICKNAME' => booking.property.nickname, 'SERVICES' => booking.services.map(&:display).join(','),
+                      'ACCESS' => booking.property.access_info, 'PARKING' => booking.property.parking_info,
+                      'TRASH' => booking.property.trash_disposal, 'RESTOCKING' => booking.property.restocking_info,
+                      'SPECIAL_INSTRUCTIONS' => booking.property.additional_info,
                       'KING' => booking.property.king_beds, 'QUEEN' => booking.property.queen_beds,
                       'FULL' => booking.property.full_beds, 'TWIN' => booking.property.twin_beds
                     },
