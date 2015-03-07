69b3b3f22248682ead7401b4cb7d8a3690d6f275
Chris Bolton <c@chrisbolton.me>
2015-03-06 18:12:56 -0800

    MandrillMailer.config

--- config/initializers/mandrill.rb
+++ config/initializers/mandrill.rb
@@ -1,1 +1,12 @@
-MandrillMailer.configure {|c| c.api_key = ENV['MANDRILL_API_KEY'] }
+MandrillMailer.configure do |c|
+  c.api_key = ENV['MANDRILL_API_KEY']
+
+  case Rails.env
+  when 'production'
+    MandrillMailer.config.default_url_options = { host: 'www.hostwise.com', protocol: 'https' }
+  when 'staging'
+    MandrillMailer.config.default_url_options = { host: 'www.hostwise-staging.com', protocol: 'http' }
+  when 'development'
+    MandrillMailer.config.default_url_options = { host: ENV['HOST'], protocol: 'http' }
+  end
+end
