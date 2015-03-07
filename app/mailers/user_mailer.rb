218ce0fb337489bfbe1cd1b839c685a628f6846a
Chris Bolton <c@chrisbolton.me>
2015-03-06 17:54:40 -0800

    property_link

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -128,7 +128,7 @@ class UserMailer < MandrillMailer::TemplateMailer
                       'ADDRESS' => property.full_address,
                       'NICKNAME' => property.nickname,
                       'PROP_SIZE' => property.property_size,
-                      'PROPERTY_LINK' => property_url property.slug
+                      'PROPERTY_LINK' => property_url(property.slug)
                     },
                     inline_css: true,
                     async: true,
