b86a00269e53ce116cb95c543b381d0bfb47c08d
Andre.Lee <andrelee082@gmail.com>
2015-03-17 00:20:18 -0700

    quiz logic.

--- config/routes.rb
+++ config/routes.rb
@@ -63,6 +63,7 @@ Rails.application.routes.draw do
     post  '/checklist' => 'jobs#checklist'
     post  '/checklist/update' => 'jobs#checklist_update' 
     get   '/quiz' => 'quiz#index'
+    post  '/quiz/report' => 'quiz#report'
   end
 
   scope module: 'admin', constraints: { subdomain: 'admin' } do
