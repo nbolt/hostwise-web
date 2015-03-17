d3bf043a308656f94156cab634b7e9ef30449694
Andre.Lee <andrelee082@gmail.com>
2015-03-17 15:02:27 -0700

    logic fix for quiz.

--- app/models/quiz_stage.rb
+++ app/models/quiz_stage.rb
@@ -1,8 +1,10 @@
 class QuizStage < ActiveRecord::Base
   belongs_to :contractor_profile
 
-  scope :last_taken, -> (contractor_profile) { where('contractor_profile_id = ? and pass = ?', contractor_profile.id, true).order('created_at DESC').first }
+  scope :passed, -> (contractor_profile) { where('contractor_profile_id = ? and pass = ?', contractor_profile.id, true).order('created_at DESC') }
 
+  # next quiz will be taken at nth job (based on last quiz taken)
+  # eg. if last quiz was taken at 2nd job, next quiz will be at 6th job
   def next
     case took_at
       when 0
@@ -17,6 +19,8 @@ class QuizStage < ActiveRecord::Base
         21
       when 21
         41
+      else
+        0
     end
   end
 end
