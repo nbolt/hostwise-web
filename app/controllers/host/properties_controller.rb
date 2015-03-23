aabcc038cd3c36c7071a0c4471fb13d444c022c4
Chris Bolton <c@chrisbolton.me>
2015-03-23 14:03:38 -0700

    hide rooms on edit property modal when bookings exist

--- app/controllers/host/properties_controller.rb
+++ app/controllers/host/properties_controller.rb
@@ -5,7 +5,7 @@ class Host::PropertiesController < Host::AuthController
   def show
     respond_to do |format|
       format.html { redirect_to '/' unless property }
-      format.json { render json: property.to_json(include: {property_photos: {}, active_bookings: {include: [:services], methods: [:cost]}, future_bookings: {include: [:services], methods: [:cost]}, past_bookings: {include: [:services], methods: [:cost]}}, methods: [:nickname, :short_address, :primary_photo, :full_address, :next_service_date, :beds]) }
+      format.json { render json: property.to_json(include: {property_photos: {}, bookings: {}, active_bookings: {include: [:services], methods: [:cost]}, future_bookings: {include: [:services], methods: [:cost]}, past_bookings: {include: [:services], methods: [:cost]}}, methods: [:nickname, :short_address, :primary_photo, :full_address, :next_service_date, :beds]) }
     end
   end
 
