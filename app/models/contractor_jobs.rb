eaf369c19bbb69cda9132ba7710a9d036f1ad738
Chris Bolton <c@chrisbolton.me>
2015-03-23 15:35:23 -0700

    one checklist per job

--- app/models/contractor_jobs.rb
+++ app/models/contractor_jobs.rb
@@ -3,5 +3,11 @@ class ContractorJobs < ActiveRecord::Base
   belongs_to :user
   has_one :checklist, foreign_key: :contractor_job_id, dependent: :destroy
 
-  after_create :create_checklist
+  after_save :create_check
+
+  private
+
+  def create_check
+    create_checklist if primary && !checklist
+  end
 end
