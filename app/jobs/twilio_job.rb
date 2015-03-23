c7186896a1f4926973fd99edb45c66c9f00fbfae
Andre.Lee <andrelee082@gmail.com>
2015-03-23 16:21:04 -0700

    mms

--- app/jobs/twilio_job.rb
+++ app/jobs/twilio_job.rb
@@ -1,14 +1,13 @@
 class TwilioJob < ActiveJob::Base
   queue_as :default
 
-  def perform(to, body)
+  def perform(to, body, photos = nil)
     twilio = Twilio::REST::Client.new(ENV['TWILIO_SID'], ENV['TWILIO_TOKEN'])
+    params = {from: '+14244445446', to: to, body: body}
+    params[:media_url] = photos if photos.present?
+
     begin
-      twilio.account.messages.create(
-        from: '+14244445446',
-        to: to,
-        body: body
-      )
+      twilio.account.messages.create params
     rescue Exception => e
       Rails.logger.error "Twilio error: #{e}"
     end
