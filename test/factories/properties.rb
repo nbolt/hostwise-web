dc3852c1d7999927df3ae755d2359ccc41ab4e83
Chris Bolton <c@chrisbolton.me>
2015-03-24 18:02:40 -0700

    timezone test fix

--- test/factories/properties.rb
+++ test/factories/properties.rb
@@ -46,8 +46,8 @@ FactoryGirl.define do
     full_beds 0
     twin_beds 1
   	property_type :house
-  	lat 37.7178216
-  	lng -121.9258115
+  	lat 53.349805
+  	lng -6.260310
     association :user, factory: :user_name_3
   end
 
