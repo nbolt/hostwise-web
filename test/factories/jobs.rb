fb5c4835545aab4d6460cba845445bba6b197576
Chris Bolton <c@chrisbolton.me>
2015-03-28 12:58:50 -0700

    minor fixes

--- test/factories/jobs.rb
+++ test/factories/jobs.rb
@@ -2,26 +2,31 @@ FactoryGirl.define do |job|
 	factory :job_1, class: Job do
 		association :booking, factory: :booking_cancelled
 		date Date.new(2015, 4, 18)
+    status_cd 1
   end
 
   factory :job_2, class: Job do
 		association :booking, factory: :booking_active_1
 		date Date.new(2015, 4, 18)
+    status_cd 1
   end
 
   factory :job_3, class: Job do
 		association :booking, factory: :booking_cancelled
 		date Date.new(2015, 4, 19)
+    status_cd 1
   end
 
   factory :job_4, class: Job do
 		association :booking, factory: :booking_cancelled
 		date Date.new(2015, 4, 20)
+    status_cd 1
   end
 
   factory :job_5, class: Job do
 		association :booking, factory: :booking_active_1
 		date Date.new(2015, 4, 18)
+    status_cd 1
 		size 2
   end
 
