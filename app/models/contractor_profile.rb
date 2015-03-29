7b22594788419d6445894bf1eada5272fe30ede2
Andre.Lee <andrelee082@gmail.com>
2015-03-28 22:11:29 -0700

    update contractor status on admin dash.

--- app/models/contractor_profile.rb
+++ app/models/contractor_profile.rb
@@ -13,7 +13,7 @@ class ContractorProfile < ActiveRecord::Base
   validates_length_of :emergency_contact_phone, is: 10, if: lambda { self.emergency_contact_phone.present? }
 
   def current_position
-    {id: position_cd.to_s, text: position.upcase}
+    {id: position_cd.to_s, text: display_position.upcase}
   end
 
   def display_position
