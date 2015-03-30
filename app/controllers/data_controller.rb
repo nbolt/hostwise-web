d22e74dd09f02c5e08bcc3a94a007200a19995b2
Chris Bolton <c@chrisbolton.me>
2015-03-30 13:25:39 -0700

    infinite job queue

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -35,18 +35,17 @@ class DataController < ApplicationController
     end
     jobs.each {|j| j.current_user = current_user}
     jobs_count = jobs.count
-    jobs = jobs.group_by{|job| job.date.strftime '%m-%d-%y'}.sort_by{|date| Date.strptime(date[0], '%m-%d-%y')}
     if params[:scope] == 'open'
-      jobs_by_week = jobs.group_by{|jobs| day = Date.strptime(jobs[0], '%m-%d-%y'); (day - day.wday).strftime('%m-%d-%y')}.sort_by{|jobs |Date.strptime(jobs[0], '%m-%d-%y')}
-      num = 0; week = []
-      while week.empty? && jobs_by_week[params[:page].to_i + num]
-        week = jobs_by_week[params[:page].to_i + num][1]
-        week = week.each {|jobs| jobs[1] = jobs[1].select{|job| !job.previous_team_job && (job.first_job_of_day || job.contractor_hours + job.man_hours <= MAX_MAN_HOURS)}}
-        week = week.select {|jobs| if jobs[1][0] then true else false end}
+      selected_jobs = []; num = 0
+      while selected_jobs.count < JOBS_PER_PAGE && jobs[num + (params[:page].to_i - 1) * JOBS_PER_PAGE]
+        job = jobs[num + (params[:page].to_i - 1) * JOBS_PER_PAGE]
+        selected_jobs.push job if !job.previous_team_job && (job.first_job_of_day || job.contractor_hours + job.man_hours <= MAX_MAN_HOURS)
         num += 1
       end
-      render json: { weeks_count: jobs_by_week.count, jobs_count: jobs_count, jobs: week.to_json(methods: [:payout, :payout_integer, :payout_fractional, :staging, :man_hours, :contractor_hours, :first_job_of_day, :previous_team_job], include: {contractors: {}, booking: {methods: :cost, include: {property: {include: {user: {methods: :name}}, methods: [:short_address, :full_address, :primary_photo, :neighborhood]}}}}) }
+      jobs = selected_jobs.group_by{|job| job.date.strftime '%m-%d-%y'}.sort_by{|date| Date.strptime(date[0], '%m-%d-%y')}
+      render json: { jobs_count: jobs_count, jobs: jobs.to_json(methods: [:payout, :payout_integer, :payout_fractional, :staging, :man_hours, :contractor_hours, :first_job_of_day, :previous_team_job], include: {contractors: {}, booking: {methods: :cost, include: {property: {include: {user: {methods: :name}}, methods: [:short_address, :full_address, :primary_photo, :neighborhood]}}}}) }
     else
+      jobs = jobs.group_by{|job| job.date.strftime '%m-%d-%y'}.sort_by{|date| Date.strptime(date[0], '%m-%d-%y')}
       render json: jobs.to_json(methods: [:payout, :payout_integer, :payout_fractional, :staging, :man_hours, :contractor_hours], include: {contractors: {}, booking: {methods: :cost, include: {property: {include: {user: {methods: :name}}, methods: [:short_address, :full_address, :primary_photo, :neighborhood]}}}})
     end
   end
