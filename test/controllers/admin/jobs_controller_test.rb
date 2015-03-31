914664003cff69b64af3e279e8d8007a9f4a3199
David Kong <david.siqi.kong@gmail.com>
2015-03-31 16:33:43 -0700

    Additional controller tests

--- test/controllers/admin/jobs_controller_test.rb
+++ test/controllers/admin/jobs_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
  
 describe Admin::JobsController do
-  it 'index' do
-
+  it 'booking_cost' do
+    booking_canceled = nil
+    VCR.use_cassette('create_booking_cancelled') { booking_cancelled = create(:booking_cancelled) }
   end
 end
