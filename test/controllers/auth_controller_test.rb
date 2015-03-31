50b2b8e008b07c778973e826905570dc2c776441
David Kong <david.siqi.kong@gmail.com>
2015-03-31 11:43:26 -0700

    start controller tests

---
+++ test/controllers/auth_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+describe Admin::AuthController do
+  it 'auth' do
+    #get :auth
+  end
+end
---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+describe HomeController do
+  it 'index' do
+    get :index
+  end
+end
