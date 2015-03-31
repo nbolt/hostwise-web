7a815e6bc6ef2161fe5ef31586d536a9c9fdc38d
Chris Bolton <c@chrisbolton.me>
2015-03-31 13:30:33 -0700

    improved job queue and other fixes

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -36,11 +36,13 @@ class DataController < ApplicationController
     jobs.each {|j| j.current_user = current_user}
     jobs_count = jobs.count
     if params[:scope] == 'open'
-      selected_jobs = []; num = 0
-      jobs[0 .. (num + (params[:page].to_i - 1) * JOBS_PER_PAGE)-1].each {|job| num += 1 unless !job.previous_team_job && !job.training && (job.first_job_of_day || job.contractor_hours + job.man_hours <= MAX_MAN_HOURS) } if params[:page].to_i > 1
-      while selected_jobs.count < JOBS_PER_PAGE && jobs[num + (params[:page].to_i - 1) * JOBS_PER_PAGE]
-        job = jobs[num + (params[:page].to_i - 1) * JOBS_PER_PAGE]
-        selected_jobs.push job if !job.previous_team_job && !job.training && (job.first_job_of_day || job.contractor_hours + job.man_hours <= MAX_MAN_HOURS)
+      selected_jobs = []; num = 0; processed = 0; offset = (params[:page].to_i - 1) * JOBS_PER_PAGE
+      while selected_jobs.count < JOBS_PER_PAGE && jobs[num]
+        job = jobs[num]
+        if !job.previous_team_job && !job.training && (job.first_job_of_day || job.contractor_hours + job.man_hours <= MAX_MAN_HOURS)
+          processed += 1
+          selected_jobs.push job if processed > offset && processed <= offset + JOBS_PER_PAGE
+        end
         num += 1
       end
       jobs = selected_jobs.group_by{|job| job.date.strftime '%m-%d-%y'}.sort_by{|date| Date.strptime(date[0], '%m-%d-%y')}
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -78,9 +78,9 @@ class Booking < ActiveRecord::Base
   def cost
     cost = Booking.cost(property, services, first_booking_discount, late_next_day, late_same_day, no_access_fee)
     if cancelled? || couldnt_access?
-      cost[:cost] -= cost[:linens] if cost[:linens]
-      cost[:cost] -= cost[:toiletries] if cost[:toiletries]
-      cost[:cost] = 0 if cost[:cost] < 0
+      cost[:cost] -= cost[:linens] if cost[:linens]          # |
+      cost[:cost] -= cost[:toiletries] if cost[:toiletries]  # |  NOTE: tests needed [mutation coverage]
+      cost[:cost] = 0 if cost[:cost] < 0                     # |
       [PRICING['cancellation'], (cost[:cost] * 0.2).round(2)].max
     else
       cost[:cost]
