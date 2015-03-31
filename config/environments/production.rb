c40434eefe64556332a47aa94da95d0cbdc735c3
Andre.Lee <andrelee082@gmail.com>
2015-03-31 10:57:26 -0700

    Update ops email.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.booking_notification_email = 'bookings@hostwise.com'
-  config.support_notification_email = 'support@hostwise.com'
+  config.support_notification_email = 'team@hostwise.com'
 
   config.quiz_num_questions = 10
 end
