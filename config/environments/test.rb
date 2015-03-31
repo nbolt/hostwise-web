22275251eec6683cc93458025c70ea60c5edb9ce
David Kong <david.siqi.kong@gmail.com>
2015-03-31 11:43:26 -0700

    start controller tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  config.action_controller.default_url_options = { host: ENV['HOST'], protocol: 'http' }
+  config.action_mailer.default_url_options     = { host: ENV['HOST'], protocol: 'http' }
+
   # Settings specified here will take precedence over those in config/application.rb.
   config.active_support.test_order = :parallel
   # The test environment is used exclusively to run your application's
---
+++ test/controllers/admin/jobs_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+ 
+describe Admin::JobsController do
+  it 'index' do
+
+  end
+end
