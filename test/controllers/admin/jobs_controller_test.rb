58459c917f24264be843bfc543e3e2a587833d20
David Kong <david.siqi.kong@gmail.com>
2015-04-01 13:18:58 -0700

    continue tests

--- test/controllers/admin/jobs_controller_test.rb
+++ test/controllers/admin/jobs_controller_test.rb
@@ -1,8 +1,20 @@
 require "test_helper"
  
 describe Admin::JobsController do
+  it 'index' do
+  end
+
   it 'booking_cost' do
     booking_canceled = nil
     VCR.use_cassette('create_booking_cancelled') { booking_cancelled = create(:booking_cancelled) }
+  end
+
+  it 'add_contractor' do
+  end
+
+  it 'remove_contractor' do
+  end
+
+  it 'add_service' do
   end
 end
