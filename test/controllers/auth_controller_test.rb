38c0634bf96242e51897651b5ba0626a8e3e1b84
David Kong <david.siqi.kong@gmail.com>
2015-04-01 16:27:57 -0700

    step.one  customer edit page

--- test/controllers/auth_controller_test.rb
+++ test/controllers/auth_controller_test.rb
@@ -4,5 +4,11 @@ describe AuthController do
   it 'auth' do
     get :auth
     assert_redirected_to '/signin'
+
+    user_name_13 = nil
+    VCR.use_cassette('create_user_name_13') { user_name_13 = create(:user_name_13) }
+    login_user(user_name_13)
+
+    #assert_redirected_to subdomain: user_name_13.role.to_s
   end
 end
