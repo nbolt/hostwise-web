625e5f470c79587c8a62d3e6b169a2fd45094a89
Chris Bolton <c@chrisbolton.me>
2015-04-04 13:48:01 -0700

    clock.rb fix

--- clock.rb
+++ clock.rb
@@ -4,49 +4,53 @@ require './config/boot'
 require './config/environment'
 
 module Clockwork
-  url = Rails.application.routes.url_helpers
-  
   handler do |job|
     case job
     when 'jobs:check_unclaimed'
+      url = Rails.application.routes.url_helpers
       Job.where(status_cd: 0).each do |job|
-        timezone = Timezone::Zone.new :zone => job.booking.property.zone
-        time = timezone.time Time.now
-        if time.hour == 17 && job.tomorrow?(time.to_date)
-          UserMailer.generic_notification("Job for tomorrow not filled - #{job.id}", "Job ##{job.id} (#{job.booking.property.nickname}) for tomorrow has not been claimed by the required number of contractors - #{url.admin_job_url(job)}").then(:deliver)
+        if job.chain(:booking, :property, :zone)
+          timezone = Timezone::Zone.new :zone => job.booking.property.zone
+          time = timezone.time Time.now
+          if time.hour == 17 && job.tomorrow?(time.to_date)
+            UserMailer.generic_notification("Job for tomorrow not filled - #{job.id}", "Job ##{job.id} (#{job.booking.property.nickname}) for tomorrow has not been claimed by the required number of contractors - #{url.admin_job_url(job)}").then(:deliver)
+          end
         end
       end
     when 'jobs:check_no_shows'
+      url = Rails.application.routes.url_helpers
       User.contractors.each do |contractor|
-        timezone = Timezone::Zone.new :zone => contractor.contractor_profile.zone
-        time = timezone.time Time.now
-        jobs_today = contractor.jobs.standard.on_date(time)
-        jobs_today.each {|j| j.current_user = contractor}
-        jobs_today = jobs_today.sort_by(&:priority)
-        jobs_today.each do |job|
-          staging = Rails.env.staging? && '[STAGING] ' || ''
-          first_job = job == jobs_today[0]
-          last_job  = job == jobs_today[-1]
-          second_to_last = if jobs_today.count > 1 then job == jobs_today[-2] else false end
+        if contractor.contractor_profile
+          timezone = Timezone::Zone.new :zone => contractor.contractor_profile.zone
+          time = timezone.time Time.now
+          jobs_today = contractor.jobs.standard.on_date(time)
+          jobs_today.each {|j| j.current_user = contractor}
+          jobs_today = jobs_today.sort_by(&:priority)
+          jobs_today.each do |job|
+            staging = Rails.env.staging? && '[STAGING] ' || ''
+            first_job = job == jobs_today[0]
+            last_job  = job == jobs_today[-1]
+            second_to_last = if jobs_today.count > 1 then job == jobs_today[-2] else false end
 
-          if first_job && job.status_cd == 1 && time.hour == 10
-            TwilioJob.perform_later("+1#{ENV['SUPPORT_NOTIFICATION_SMS']}", "#{staging}#{contractor.name} (#{contractor.id}) has not arrived at job ##{job.id} (#{job.booking.property.nickname})")
-            UserMailer.generic_notification("Contractor has not arrived - #{contractor.name}", "#{contractor.name} (#{contractor.id}) has not arrived at job ##{job.id} (#{job.booking.property.nickname}) - #{url.admin_job_url(job)}").then(:deliver)
-          end
+            if first_job && job.status_cd == 1 && time.hour == 10
+              TwilioJob.perform_later("+1#{ENV['SUPPORT_NOTIFICATION_SMS']}", "#{staging}#{contractor.name} (#{contractor.id}) has not arrived at job ##{job.id} (#{job.booking.property.nickname})")
+              UserMailer.generic_notification("Contractor has not arrived - #{contractor.name}", "#{contractor.name} (#{contractor.id}) has not arrived at job ##{job.id} (#{job.booking.property.nickname}) - #{url.admin_job_url(job)}").then(:deliver)
+            end
 
-          if second_to_last && job.not_complete? && time.hour == 13
-            TwilioJob.perform_later("+1#{ENV['SUPPORT_NOTIFICATION_SMS']}", "#{staging}#{contractor.name} (#{contractor.id}) has not completed job ##{job.id} (#{job.booking.property.nickname}) by 1:30p")
-            UserMailer.generic_notification("Contractor has not completed job - #{contractor.name}", "#{contractor.name} (#{contractor.id}) has not completed job ##{job.id} (#{job.booking.property.nickname}) by 1:30p - #{url.admin_job_url(job)}").then(:deliver)
-          end
+            if second_to_last && job.not_complete? && time.hour == 13
+              TwilioJob.perform_later("+1#{ENV['SUPPORT_NOTIFICATION_SMS']}", "#{staging}#{contractor.name} (#{contractor.id}) has not completed job ##{job.id} (#{job.booking.property.nickname}) by 1:30p")
+              UserMailer.generic_notification("Contractor has not completed job - #{contractor.name}", "#{contractor.name} (#{contractor.id}) has not completed job ##{job.id} (#{job.booking.property.nickname}) by 1:30p - #{url.admin_job_url(job)}").then(:deliver)
+            end
 
-          if last_job && job.status_cd == 1 && time.hour == 14
-            TwilioJob.perform_later("+1#{ENV['SUPPORT_NOTIFICATION_SMS']}", "#{staging}#{contractor.name} (#{contractor.id}) has not arrived at job ##{job.id} (#{job.booking.property.nickname})")
-            UserMailer.generic_notification("Contractor has not arrived - #{contractor.name}", "#{contractor.name} (#{contractor.id}) has not arrived at job ##{job.id} (#{job.booking.property.nickname}) - #{url.admin_job_url(job)}").then(:deliver)
-          end
+            if last_job && job.status_cd == 1 && time.hour == 14
+              TwilioJob.perform_later("+1#{ENV['SUPPORT_NOTIFICATION_SMS']}", "#{staging}#{contractor.name} (#{contractor.id}) has not arrived at job ##{job.id} (#{job.booking.property.nickname})")
+              UserMailer.generic_notification("Contractor has not arrived - #{contractor.name}", "#{contractor.name} (#{contractor.id}) has not arrived at job ##{job.id} (#{job.booking.property.nickname}) - #{url.admin_job_url(job)}").then(:deliver)
+            end
 
-          if job.not_complete? && time.hour == 22
-            job.past_due!
-            job.save
+            if job.not_complete? && time.hour == 22
+              job.past_due!
+              job.save
+            end
           end
         end
       end
