057f2c01bc97c36c22475825a4031511f7f0513c
David Kong <david.siqi.kong@gmail.com>
2015-04-06 13:05:55 -0700

    host home controller coverage

--- test/controllers/auth_controller_test.rb
+++ test/controllers/auth_controller_test.rb
@@ -8,13 +8,21 @@ describe AuthController do
     user_name_13 = nil
     VCR.use_cassette('create_user_name_13') { user_name_13 = create(:user_name_13) }
     login_user(user_name_13)
-
-    :subdomain.must_equal 'test'
   end
 
-  it 'phone_confirmed do' do
+  it 'phone_confirmed' do
     user_name_13 = nil
     VCR.use_cassette('create_user_name_13') { user_name_13 = create(:user_name_13) }
     get(:phone_confirmed, :email => 'dustinj593@gmail.com')
+  end
+
+  it 'signup' do
+    user_name_13 = nil
+    VCR.use_cassette('create_user_name_13') { user_name_13 = create(:user_name_13) }
+    user_name_13.phone_confirmed = true
+    user_name_13.save
+  end
+
+  it 'signin' do
   end
 end
