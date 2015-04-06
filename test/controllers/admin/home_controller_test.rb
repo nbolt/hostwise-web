8679adc003c5162c8337a7ccd9c177c599d7a5f4
David Kong <david.siqi.kong@gmail.com>
2015-04-06 15:36:35 -0700

    more tests

---
+++ test/controllers/admin/home_controller_test.rb
@@ -0,0 +1,10 @@
+describe Admin::HomeController do 
+  it 'index' do
+    user_name_11 = nil
+    VCR.use_cassette('create_user_name_11') { user_name_11 = create(:user_name_11) }
+    login_user(user_name_11)
+
+    get :index
+    assert_redirected_to  '/jobs'
+  end
+end
