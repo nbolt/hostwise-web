2b05e96703c09b645e552cbdc152bff4f009bd1d
Chris Bolton <c@chrisbolton.me>
2015-04-14 12:20:03 -0700

    removing binding.pry

--- test/controllers/host/payments_controller_test.rb
+++ test/controllers/host/payments_controller_test.rb
@@ -6,7 +6,6 @@ describe Host::PaymentsController do
     credit_card = create(:credit_card)
     VCR.use_cassette('create_user_name_14') { user_name_14 = create(:user_name_14) }
     login_user(user_name_14)
-    binding.pry
     get(:add, :id => credit_card.id)
 
     assert_response :success
