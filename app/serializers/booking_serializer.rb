61b8f0969293f90d3c1a881f9e69ffbaac1901e7
David Kong <david.siqi.kong@gmail.com>
2015-04-15 13:00:53 -0700

    add booking status_cd to serializer

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -1,5 +1,5 @@
 class BookingSerializer < ActiveModel::Serializer
-  attributes :id, :property_id, :cost, :original_cost, :adjusted, :late_same_day, :late_next_day
+  attributes :id, :property_id, :cost, :original_cost, :adjusted, :late_same_day, :late_next_day, :status_cd
 
   has_one :property
   has_one :user
