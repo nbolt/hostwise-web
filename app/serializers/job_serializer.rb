6e33d9d43c750999ec4d86d70ea10263f8fc073a
Chris Bolton <c@chrisbolton.me>
2015-04-20 09:39:06 -0700

    json update

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,5 +1,5 @@
 class JobSerializer < ActiveModel::Serializer
-  attributes :id, :status_cd, :state_cd, :date, :staging, :distribution, :king_beds, :queen_beds, :full_beds, :twin_beds, :toiletries, :payout, :payout_integer, :payout_fractional
+  attributes :id, :status_cd, :state_cd, :date, :staging, :distribution, :king_beds, :queen_beds, :full_beds, :twin_beds, :toiletries, :contractor_names
 
   has_many :contractors
   has_many :payouts
