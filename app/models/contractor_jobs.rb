442ec084b10127738a819d0ded09e02385751e8b
Chris Bolton <c@chrisbolton.me>
2015-04-21 15:59:30 -0700

    create_checklist

--- app/models/contractor_jobs.rb
+++ app/models/contractor_jobs.rb
@@ -3,11 +3,5 @@ class ContractorJobs < ActiveRecord::Base
   belongs_to :user
   has_one :checklist, foreign_key: :contractor_job_id, dependent: :destroy
 
-  after_save :create_check
-
-  private
-
-  def create_check
-    create_checklist if primary && !checklist
-  end
+  after_create :create_checklist
 end
