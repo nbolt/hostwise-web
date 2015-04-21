f0887f0142dd9989944f2a926c50762b3075e6cd
Chris Bolton <c@chrisbolton.me>
2015-04-21 13:22:47 -0700

    verify_stripe!

--- app/controllers/contractor/payments_controller.rb
+++ app/controllers/contractor/payments_controller.rb
@@ -6,6 +6,7 @@ class Contractor::PaymentsController < Contractor::AuthController
       if !params[:payment_method] || params[:payment_method][:id] == 'bank-account'
         recipient.bank_account = params[:stripe_id]
         recipient.save
+        current_user.contractor_profile.verify_stripe! if recipient.verification.fields_needed[0]
         bank = recipient.bank_accounts.data[0]
         current_user.payments.destroy_all
         current_user.payments.create(bank_name: bank.bank_name, last4: bank.last4, routing_number: bank.routing_number, fingerprint: bank.fingerprint, stripe_id: bank.id, primary: true, status_cd: 1)
