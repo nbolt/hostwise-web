3d0c2df99630f31c79faeae35e85eb359f6043ac
Chris Bolton <c@chrisbolton.me>
2015-04-28 15:53:30 -0700

    no distribution jobs

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,25 +3,25 @@ class Admin::DashboardController < Admin::AuthController
 
   def revenue
     data  = Job.revenue_by_months(Date.today)
-    total = Job.where('status_cd > 2').reduce(0) {|acc, job| acc + (job.chain(:booking, :cost) || 0)}
+    total = Job.standard.where('status_cd > 2').reduce(0) {|acc, job| acc + (job.chain(:booking, :cost) || 0)}
     render json: { data: data, total: number_with_precision(total, precision: 2, delimiter: ',') }
   end
 
   def serviced
     data  = Job.serviced_by_months(Date.today)
-    total = Job.where('status_cd > 2').count
+    total = Job.standard.complete.count
     render json: { data: data, total: number_with_delimiter(total) }
   end
 
   def properties
     data  = Job.properties_by_months(Date.today)
-    total = Job.where('jobs.status_cd > 2').select('distinct bookings.property_id').includes(:booking).references(:bookings).count
+    total = Job.standard.complete.select('distinct bookings.property_id').includes(:booking).references(:bookings).count
     render json: { data: data, total: number_with_delimiter(total) }
   end
 
   def hosts
     data  = Job.hosts_by_months(Date.today)
-    total = Job.where('jobs.status_cd > 2').select('distinct properties.user_id').includes(booking: [:property]).references(:properties).count
+    total = Job.standard.complete.select('distinct properties.user_id').includes(booking: [:property]).references(:properties).count
     render json: { data: data, total: number_with_delimiter(total) }
   end
 
