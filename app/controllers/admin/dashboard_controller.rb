82cf1293d8654132b0ee3a4fb72419022d6f049c
Chris Bolton <c@chrisbolton.me>
2015-04-28 15:25:36 -0700

    chart updates

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,23 +2,27 @@ class Admin::DashboardController < Admin::AuthController
   include ActionView::Helpers::NumberHelper
 
   def revenue
-    data = Job.revenue_by_months(Date.today)
-    render json: { data: data, average: number_with_delimiter(data.reduce(0) {|acc, r| acc + r[:revenue]} / data.count) }
+    data  = Job.revenue_by_months(Date.today)
+    total = Job.where('status_cd > 2').reduce(0) {|acc, job| acc + (job.chain(:booking, :cost) || 0)}
+    render json: { data: data, total: number_with_precision(total, precision: 2, delimiter: ',') }
   end
 
   def serviced
-    data = Job.serviced_by_months(Date.today)
-    render json: { data: data, average: number_with_delimiter(data.reduce(0) {|acc, r| acc + r[:serviced]} / data.count) }
+    data  = Job.serviced_by_months(Date.today)
+    total = Job.where('status_cd > 2').count
+    render json: { data: data, total: number_with_delimiter(total) }
   end
 
   def properties
-    data = Job.properties_by_months(Date.today)
-    render json: { data: data, average: number_with_delimiter(data.reduce(0) {|acc, r| acc + r[:properties]} / data.count) }
+    data  = Job.properties_by_months(Date.today)
+    total = Job.where('jobs.status_cd > 2').select('distinct bookings.property_id').includes(:booking).references(:bookings).count
+    render json: { data: data, total: number_with_delimiter(total) }
   end
 
   def hosts
-    data = Job.hosts_by_months(Date.today)
-    render json: { data: data, average: number_with_delimiter(data.reduce(0) {|acc, r| acc + r[:hosts]} / data.count) }
+    data  = Job.hosts_by_months(Date.today)
+    total = Job.where('jobs.status_cd > 2').select('distinct properties.user_id').includes(booking: [:property]).references(:properties).count
+    render json: { data: data, total: number_with_delimiter(total) }
   end
 
 end
