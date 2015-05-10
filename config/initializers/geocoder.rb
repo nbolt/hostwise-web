bce2e64d5ffe146f73b1d75d686dde4d28025f6b
Chris Bolton <c@chrisbolton.me>
2015-05-09 18:19:41 -0700

    openstreetmaps

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,4 +1,1 @@
-Geocoder.configure(
-  lookup: :bing,
-  api_key: 'AsoGqLb3oP3iHvJIdBZi4bbKSpNTKGCZZT3GhSE3dk3O9cmXnc9_dZIx0vDXuW5u'
-)
+Geocoder.configure(lookup: :nominatim, http_headers: { "User-Agent" => "www.hostwise.com" })
