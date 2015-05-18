d20f26496228f599fba2fc17475946813eea2833
Chris Bolton <c@chrisbolton.me>
2015-05-18 11:02:33 -0700

    csv fix

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -73,6 +73,7 @@ class DataController < ApplicationController
         bookings = Booking.complete current_user
         respond_to do |format|
           format.json { render json: bookings.to_json(methods: :cost, include: [:payment, :services, property: {methods: :nickname}]) }
+          format.csv { send_data transaction_csv(bookings), filename: "completed_transactions_#{params[:start_date].gsub('/', '_')}_#{params[:end_date].gsub('/', '_')}.csv" }
         end
         #transactions = Transaction.completed(current_user, params[:start_date], params[:end_date])
         #respond_to do |format|
--- app/helpers/csv_helper.rb
+++ app/helpers/csv_helper.rb
@@ -4,13 +4,13 @@ module CsvHelper
   def transaction_csv(results)
     CSV.generate do |csv|
       csv << %w(Date Property Services Payment Total)
-      results.each do |transaction|
-        booking = transaction.bookings.find {|booking| booking.user.id == current_user.id}
+      results.each do |booking|
+        #booking = transaction.bookings.find {|booking| booking.user.id == current_user.id}
         csv << [booking.date.strftime('%m/%d/%Y'),
                 booking.property.nickname,
                 booking.services.collect{|s| s.display}.join(','),
                 "**** #{booking.payment.last4}",
-                "#{number_to_currency(transaction.amount / 100, precision: 2)}"]
+                "#{number_to_currency(booking.cost, precision: 2)}"]
       end
     end
   end
