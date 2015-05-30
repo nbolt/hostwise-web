39376a26c715e73cb0d8d95b5bd7781088b3fc61
Chris Bolton <c@chrisbolton.me>
2015-05-29 18:20:14 -0700

    job queue ui update

--- app/serializers/data_jobs_serializer.rb
+++ app/serializers/data_jobs_serializer.rb
@@ -1,5 +1,5 @@
 class DataJobsSerializer < ActiveModel::Serializer
-  attributes :id, :state_cd, :date, :size, :priority, :payout, :payout_integer, :payout_fractional, :staging, :man_hours, :contractor_hours
+  attributes :id, :state_cd, :date, :size, :priority, :payout, :payout_integer, :payout_fractional, :staging, :man_hours, :contractor_hours, :formatted_time
 
   has_one :booking, serializer: DataBookingSerializer
 end
