8b75a12df59d9b29dc1683023cf1b1f8d31e424b
Chris Bolton <c@chrisbolton.me>
2015-06-07 21:57:59 -0700

    auth fix

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -1,7 +1,7 @@
 class Api::AuthController < ApplicationController
 
   def login
-    user = User.where(email: params[:email])[0]
+    user = User.where(email: params[:email].gsub(' ', '+'))[0]
     if user && user.valid_password?(params[:password])
       render json: { token: user.auth_token, id: user.id }
     else
