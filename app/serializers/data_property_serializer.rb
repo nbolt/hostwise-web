0a999fd01e0ef2b339d0faf5fb70e87b744f30cd
Andre.Lee <andrelee082@gmail.com>
2015-06-30 00:19:40 -0700

    ui fix

--- app/serializers/data_property_serializer.rb
+++ app/serializers/data_property_serializer.rb
@@ -1,5 +1,5 @@
 class DataPropertySerializer < ActiveModel::Serializer
-  attributes :id, :property_type_cd, :linen_handling_cd, :property_size, :nickname, :short_address, :full_address, :neighborhood, :primary_photo
+  attributes :id, :property_type_cd, :linen_handling_cd, :property_size, :rooms, :nickname, :short_address, :full_address, :neighborhood, :primary_photo
 
   has_one :user, serializer: DataUserSerializer
 end
