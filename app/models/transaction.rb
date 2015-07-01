2ebf292ed1cb42e723c4468dc87b29eee4305244
Chris Bolton <c@chrisbolton.me>
2015-07-01 23:52:53 +0200

    transaction.completed fix

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -14,7 +14,7 @@ class Transaction < ActiveRecord::Base
 
   def self.completed(user, start_date, end_date)
     if start_date.present? && end_date.present?
-      Transaction.where(status_cd: 0).where('bookings.date >= ? and bookings.date <= ?', DateTime.strptime(start_date, '%m/%d/%Y'), DateTime.strptime(end_date, '%m/%d/%Y')).includes({bookings: [{property: :user}]}).references(:user, :bookings).where('users.id = ?', user.id)
+      Transaction.where(status_cd: 0).where('transactions.created_at >= ? and transactions.created_at <= ?', DateTime.strptime(start_date, '%m/%d/%Y'), DateTime.strptime(end_date, '%m/%d/%Y')).includes({bookings: [{property: :user}]}).references(:user, :bookings).where('users.id = ?', user.id)
     else
       Transaction.where(status_cd: 0).includes({bookings: [{property: :user}]}).references(:user).where('users.id = ?', user.id)
     end
