372672ac004298f6d5c0a043efa546be2ec371c3
Chris Bolton <c@chrisbolton.me>
2015-07-01 11:57:01 +0200

    blast fix

--- app/controllers/host/properties_controller.rb
+++ app/controllers/host/properties_controller.rb
@@ -341,8 +341,10 @@ class Host::PropertiesController < Host::AuthController
   def blast(booking)
     if booking.job
       User.available_contractors(booking).each do |contractor|
-        UserMailer.new_open_job(contractor, booking.job).then(:deliver) if contractor.settings(:new_open_job).email
-        TwilioJob.perform_later("+1#{contractor.phone_number}", "New HostWise Job! $#{booking.job.payout(contractor)} in #{booking.property.city}, #{booking.property.zip} on #{booking.formatted_date}.") if contractor.settings(:new_open_job).sms
+        if contractor.can_claim_job?(booking.job)[:success]
+          UserMailer.new_open_job(contractor, booking.job).then(:deliver) if contractor.settings(:new_open_job).email
+          TwilioJob.perform_later("+1#{contractor.phone_number}", "New HostWise Job! $#{booking.job.payout(contractor)} in #{booking.property.city}, #{booking.property.zip} on #{booking.formatted_date}.") if contractor.settings(:new_open_job).sms
+        end
       end
     end
   end
