c93d24d9dcd2cb576df7ef080fe2d5d79e718d45
Chris Bolton <c@chrisbolton.me>
2015-07-01 12:07:43 +0200

    connection pool

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20150630182135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_stat_statements"
 
   create_table "availabilities", force: :cascade do |t|
     t.integer  "user_id"
@@ -119,17 +120,17 @@ ActiveRecord::Schema.define(version: 20150630182135) do
     t.integer  "discounted_cost",             default: 0
     t.string   "discounted_reason",           default: ""
     t.string   "overage_reason",              default: ""
-    t.boolean  "refunded",                    default: false
-    t.integer  "refunded_cost",               default: 0
-    t.string   "refunded_reason"
-    t.string   "stripe_refund_id"
     t.integer  "extra_king_sets",             default: 0
     t.integer  "extra_twin_sets",             default: 0
     t.integer  "extra_toiletry_sets",         default: 0
     t.string   "extra_instructions",          default: ""
+    t.boolean  "refunded",                    default: false
+    t.integer  "refunded_cost",               default: 0
+    t.string   "refunded_reason"
     t.integer  "extra_king_sets_cost",        default: 0
     t.integer  "extra_twin_sets_cost",        default: 0
     t.integer  "extra_toiletry_sets_cost",    default: 0
+    t.string   "stripe_refund_id"
     t.integer  "coupon_cost",                 default: 0
     t.integer  "contractor_service_cost",     default: 0
     t.integer  "timeslot_cost"
@@ -184,7 +185,7 @@ ActiveRecord::Schema.define(version: 20150630182135) do
 
   create_table "cities", force: :cascade do |t|
     t.integer  "county_id"
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -259,7 +260,7 @@ ActiveRecord::Schema.define(version: 20150630182135) do
 
   create_table "counties", force: :cascade do |t|
     t.integer  "state_id"
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -334,6 +335,7 @@ ActiveRecord::Schema.define(version: 20150630182135) do
     t.integer  "state_cd",              default: 0
     t.integer  "occasion_cd"
     t.datetime "cant_access"
+    t.float    "man_hours"
     t.integer  "king_sheets",           default: 0
     t.integer  "twin_sheets",           default: 0
     t.integer  "pillow_count",          default: 0
@@ -341,7 +343,6 @@ ActiveRecord::Schema.define(version: 20150630182135) do
     t.integer  "hand_towels",           default: 0
     t.integer  "face_towels",           default: 0
     t.integer  "bath_mats",             default: 0
-    t.float    "man_hours"
     t.integer  "distribution_timeslot"
     t.boolean  "admin_set",             default: false
   end
@@ -410,13 +411,13 @@ ActiveRecord::Schema.define(version: 20150630182135) do
 
   create_table "properties", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "title",                  limit: 255
-    t.string   "address1",               limit: 255
-    t.string   "address2",               limit: 255
-    t.string   "city",                   limit: 255
-    t.string   "state",                  limit: 255
-    t.string   "zip",                    limit: 255
-    t.string   "country",                limit: 255
+    t.string   "title"
+    t.string   "address1"
+    t.string   "address2"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.string   "country"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "delivery_point_barcode"
@@ -431,7 +432,7 @@ ActiveRecord::Schema.define(version: 20150630182135) do
     t.integer  "full_beds"
     t.integer  "queen_beds"
     t.integer  "king_beds"
-    t.boolean  "active",                             default: false
+    t.boolean  "active",                 default: false
     t.string   "phone_number"
     t.integer  "rental_type_cd"
     t.integer  "property_type_cd"
@@ -442,7 +443,7 @@ ActiveRecord::Schema.define(version: 20150630182135) do
     t.integer  "zip_id"
     t.integer  "linen_handling_cd"
     t.date     "purchase_date"
-    t.integer  "linen_count",                        default: 0
+    t.integer  "linen_count",            default: 0
   end
 
   add_index "properties", ["zip_id"], name: "index_properties_on_zip_id", using: :btree
@@ -503,8 +504,8 @@ ActiveRecord::Schema.define(version: 20150630182135) do
   add_index "settings", ["target_type", "target_id", "var"], name: "index_settings_on_target_type_and_target_id_and_var", unique: true, using: :btree
 
   create_table "states", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "abbr",       limit: 255
+    t.string   "name"
+    t.string   "abbr"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -528,23 +529,23 @@ ActiveRecord::Schema.define(version: 20150630182135) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                           limit: 255,                 null: false
-    t.string   "crypted_password",                limit: 255,                 null: false
-    t.string   "salt",                            limit: 255,                 null: false
+    t.string   "email",                                           null: false
+    t.string   "crypted_password",                                null: false
+    t.string   "salt",                                            null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "remember_me_token",               limit: 255
+    t.string   "remember_me_token"
     t.datetime "remember_me_token_expires_at"
     t.datetime "last_login_at"
     t.datetime "last_logout_at"
     t.datetime "last_activity_at"
-    t.string   "last_login_from_ip_address",      limit: 255
-    t.string   "first_name",                      limit: 255
-    t.string   "last_name",                       limit: 255
-    t.string   "phone_number",                    limit: 255
-    t.boolean  "phone_confirmed",                             default: false
-    t.string   "company",                         limit: 255
-    t.string   "phone_confirmation",              limit: 255
+    t.string   "last_login_from_ip_address"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone_number"
+    t.boolean  "phone_confirmed",                 default: false
+    t.string   "company"
+    t.string   "phone_confirmation"
     t.string   "stripe_customer_id"
     t.string   "reset_password_token"
     t.datetime "reset_password_token_expires_at"
@@ -554,11 +555,11 @@ ActiveRecord::Schema.define(version: 20150630182135) do
     t.string   "activation_state"
     t.string   "activation_token"
     t.datetime "activation_token_expires_at"
-    t.boolean  "migrated",                                    default: false
-    t.integer  "vip_count",                                   default: 0
+    t.boolean  "migrated",                        default: false
+    t.integer  "vip_count",                       default: 0
     t.integer  "booking_count"
     t.string   "auth_token"
-    t.integer  "last_minute_cancellation_count",              default: 0
+    t.integer  "last_minute_cancellation_count",  default: 0
     t.integer  "market_id"
   end
 
@@ -571,12 +572,12 @@ ActiveRecord::Schema.define(version: 20150630182135) do
 
   create_table "zips", force: :cascade do |t|
     t.integer  "city_id"
-    t.string   "code",            limit: 255
+    t.string   "code"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "serviced",                    default: false
+    t.boolean  "serviced",        default: false
     t.integer  "neighborhood_id"
-    t.boolean  "restricted",                  default: false
+    t.boolean  "restricted",      default: false
     t.integer  "market_id"
   end
 
