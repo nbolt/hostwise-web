fb5cf57e7eb596cd94f0b4e300c79323db250179
Chris Bolton <c@chrisbolton.me>
2015-07-02 02:33:01 +0200

    add document photo to contractor detail page

--- app/serializers/contractor_profile_serializer.rb
+++ app/serializers/contractor_profile_serializer.rb
@@ -1,7 +1,7 @@
 class ContractorProfileSerializer < ActiveModel::Serializer
   attributes :address1, :address2, :city, :country, :current_position, :dob, :docusign_completed, :driver_license,
              :emergency_contact_first_name, :emergency_contact_last_name, :emergency_contact_phone, :lat, :lng,
-             :position, :position_cd, :ssn, :state, :test_session_completed, :user_id, :zip, :zone, :market_hash
+             :position, :position_cd, :ssn, :state, :test_session_completed, :user_id, :zip, :zone, :market_hash, :document
 
   has_one :market
 end
