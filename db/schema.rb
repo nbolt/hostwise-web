15b0e8865e9ed098dc44c2942704a7a09323f2d5
Chris Bolton <c@chrisbolton.me>
2015-07-21 23:33:38 +0900

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630182135) do
+ActiveRecord::Schema.define(version: 20150721052453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_stat_statements"
 
   create_table "availabilities", force: :cascade do |t|
     t.integer  "user_id"
@@ -120,17 +119,17 @@ ActiveRecord::Schema.define(version: 20150630182135) do
     t.integer  "discounted_cost",             default: 0
     t.string   "discounted_reason",           default: ""
     t.string   "overage_reason",              default: ""
+    t.boolean  "refunded",                    default: false
+    t.integer  "refunded_cost",               default: 0
+    t.string   "refunded_reason"
+    t.string   "stripe_refund_id"
     t.integer  "extra_king_sets",             default: 0
     t.integer  "extra_twin_sets",             default: 0
     t.integer  "extra_toiletry_sets",         default: 0
     t.string   "extra_instructions",          default: ""
-    t.boolean  "refunded",                    default: false
-    t.integer  "refunded_cost",               default: 0
-    t.string   "refunded_reason"
     t.integer  "extra_king_sets_cost",        default: 0
     t.integer  "extra_twin_sets_cost",        default: 0
     t.integer  "extra_toiletry_sets_cost",    default: 0
-    t.string   "stripe_refund_id"
     t.integer  "coupon_cost",                 default: 0
     t.integer  "contractor_service_cost",     default: 0
     t.integer  "timeslot_cost"
@@ -163,13 +162,14 @@ ActiveRecord::Schema.define(version: 20150630182135) do
     t.integer  "status_cd"
     t.integer  "source_cd"
     t.boolean  "super_host"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.string   "address"
     t.string   "property_type"
-    t.integer  "num_bedrooms",  default: 0
-    t.integer  "num_bathrooms", default: 0
-    t.integer  "num_beds",      default: 0
+    t.integer  "num_bedrooms",   default: 0
+    t.integer  "num_bathrooms",  default: 0
+    t.integer  "num_beds",       default: 0
+    t.date     "last_contacted"
   end
 
   create_table "checklists", force: :cascade do |t|
@@ -185,7 +185,7 @@ ActiveRecord::Schema.define(version: 20150630182135) do
 
   create_table "cities", force: :cascade do |t|
     t.integer  "county_id"
-    t.string   "name"
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -260,7 +260,7 @@ ActiveRecord::Schema.define(version: 20150630182135) do
 
   create_table "counties", force: :cascade do |t|
     t.integer  "state_id"
-    t.string   "name"
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -321,30 +321,33 @@ ActiveRecord::Schema.define(version: 20150630182135) do
   create_table "jobs", force: :cascade do |t|
     t.integer  "status_cd"
     t.integer  "booking_id"
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
-    t.boolean  "distribution",          default: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
+    t.boolean  "distribution",            default: false
     t.integer  "king_beds"
     t.integer  "queen_beds"
     t.integer  "full_beds"
     t.integer  "twin_beds"
-    t.integer  "size",                  default: 1
+    t.integer  "size",                    default: 1
     t.integer  "toiletries"
-    t.boolean  "training",              default: false
+    t.boolean  "training",                default: false
     t.date     "date"
-    t.integer  "state_cd",              default: 0
+    t.integer  "state_cd",                default: 0
     t.integer  "occasion_cd"
     t.datetime "cant_access"
+    t.integer  "king_sheets",             default: 0
+    t.integer  "twin_sheets",             default: 0
+    t.integer  "pillow_count",            default: 0
+    t.integer  "bath_towels",             default: 0
+    t.integer  "hand_towels",             default: 0
+    t.integer  "face_towels",             default: 0
+    t.integer  "bath_mats",               default: 0
     t.float    "man_hours"
-    t.integer  "king_sheets",           default: 0
-    t.integer  "twin_sheets",           default: 0
-    t.integer  "pillow_count",          default: 0
-    t.integer  "bath_towels",           default: 0
-    t.integer  "hand_towels",           default: 0
-    t.integer  "face_towels",           default: 0
-    t.integer  "bath_mats",             default: 0
     t.integer  "distribution_timeslot"
-    t.boolean  "admin_set",             default: false
+    t.boolean  "admin_set",               default: false
+    t.integer  "king_sets_picked_up",     default: 0
+    t.integer  "twin_sets_picked_up",     default: 0
+    t.integer  "toiletry_sets_picked_up", default: 0
   end
 
   create_table "markets", force: :cascade do |t|
@@ -411,13 +414,13 @@ ActiveRecord::Schema.define(version: 20150630182135) do
 
   create_table "properties", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "title"
-    t.string   "address1"
-    t.string   "address2"
-    t.string   "city"
-    t.string   "state"
-    t.string   "zip"
-    t.string   "country"
+    t.string   "title",                  limit: 255
+    t.string   "address1",               limit: 255
+    t.string   "address2",               limit: 255
+    t.string   "city",                   limit: 255
+    t.string   "state",                  limit: 255
+    t.string   "zip",                    limit: 255
+    t.string   "country",                limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "delivery_point_barcode"
@@ -432,7 +435,7 @@ ActiveRecord::Schema.define(version: 20150630182135) do
     t.integer  "full_beds"
     t.integer  "queen_beds"
     t.integer  "king_beds"
-    t.boolean  "active",                 default: false
+    t.boolean  "active",                             default: false
     t.string   "phone_number"
     t.integer  "rental_type_cd"
     t.integer  "property_type_cd"
@@ -443,7 +446,7 @@ ActiveRecord::Schema.define(version: 20150630182135) do
     t.integer  "zip_id"
     t.integer  "linen_handling_cd"
     t.date     "purchase_date"
-    t.integer  "linen_count",            default: 0
+    t.integer  "linen_count",                        default: 0
   end
 
   add_index "properties", ["zip_id"], name: "index_properties_on_zip_id", using: :btree
@@ -504,8 +507,8 @@ ActiveRecord::Schema.define(version: 20150630182135) do
   add_index "settings", ["target_type", "target_id", "var"], name: "index_settings_on_target_type_and_target_id_and_var", unique: true, using: :btree
 
   create_table "states", force: :cascade do |t|
-    t.string   "name"
-    t.string   "abbr"
+    t.string   "name",       limit: 255
+    t.string   "abbr",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -529,23 +532,23 @@ ActiveRecord::Schema.define(version: 20150630182135) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                                           null: false
-    t.string   "crypted_password",                                null: false
-    t.string   "salt",                                            null: false
+    t.string   "email",                           limit: 255,                 null: false
+    t.string   "crypted_password",                limit: 255,                 null: false
+    t.string   "salt",                            limit: 255,                 null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "remember_me_token"
+    t.string   "remember_me_token",               limit: 255
     t.datetime "remember_me_token_expires_at"
     t.datetime "last_login_at"
     t.datetime "last_logout_at"
     t.datetime "last_activity_at"
-    t.string   "last_login_from_ip_address"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "phone_number"
-    t.boolean  "phone_confirmed",                 default: false
-    t.string   "company"
-    t.string   "phone_confirmation"
+    t.string   "last_login_from_ip_address",      limit: 255
+    t.string   "first_name",                      limit: 255
+    t.string   "last_name",                       limit: 255
+    t.string   "phone_number",                    limit: 255
+    t.boolean  "phone_confirmed",                             default: false
+    t.string   "company",                         limit: 255
+    t.string   "phone_confirmation",              limit: 255
     t.string   "stripe_customer_id"
     t.string   "reset_password_token"
     t.datetime "reset_password_token_expires_at"
@@ -555,11 +558,11 @@ ActiveRecord::Schema.define(version: 20150630182135) do
     t.string   "activation_state"
     t.string   "activation_token"
     t.datetime "activation_token_expires_at"
-    t.boolean  "migrated",                        default: false
-    t.integer  "vip_count",                       default: 0
+    t.boolean  "migrated",                                    default: false
+    t.integer  "vip_count",                                   default: 0
     t.integer  "booking_count"
     t.string   "auth_token"
-    t.integer  "last_minute_cancellation_count",  default: 0
+    t.integer  "last_minute_cancellation_count",              default: 0
     t.integer  "market_id"
   end
 
@@ -572,12 +575,12 @@ ActiveRecord::Schema.define(version: 20150630182135) do
 
   create_table "zips", force: :cascade do |t|
     t.integer  "city_id"
-    t.string   "code"
+    t.string   "code",            limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "serviced",        default: false
+    t.boolean  "serviced",                    default: false
     t.integer  "neighborhood_id"
-    t.boolean  "restricted",      default: false
+    t.boolean  "restricted",                  default: false
     t.integer  "market_id"
   end
 
